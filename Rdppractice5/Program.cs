0f282b825794bbcb9be1987cba56cfa8ab61526e
lipsa7 <lipsakar98@gmail.com>
2021-07-28 14:32:37 +0530

    Add project files.

---
+++ Rdppractice5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(UserProgramCode.findLargestNumber(num));
+
+            Console.ReadKey();
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static int findLargestNumber(int num)
+        {
+            int[] digits = num.ToString().ToCharArray().Select(Convert.ToInt32).ToArray();
+
+            int max = digits[0];
+            for(int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] > max)
+                {
+                    max = digits[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
